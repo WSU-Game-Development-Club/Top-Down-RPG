1f330797ddadd9373c6c54d177a9b8bcc81ed6bd
martinhundrup <hundrupm@gmail.com>
2024-10-14 07:27:54 -0700

    refactored player controller.

--- Topdown_RPG/Assets/Scripts/Player/PlayerController.cs
+++ Topdown_RPG/Assets/Scripts/Player/PlayerController.cs
@@ -2,28 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// In charge of controlling player movement
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 movement;
+    // the player's speed in units/second
+    [SerializeField] private float speed;
 
-    void Start()
+    // reference to game object's rigidbody behavior
+    private Rigidbody2D rb;
+
+    // called when script is loaded into memory
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    // called once a game tick
+    private void Update()
     {
-        // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        
+        // note: using GetAxis() allows use to have some smoothing and damping 
+        // built into the input manager, can change to GetAxisRaw() for snappier movement
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")).normalized * speed;
     }
 
-    void FixedUpdate()
-    {
-        // Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-    }
 }
