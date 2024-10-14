325c4730a46deac6d992cb348c99748f8844fc77
Oboczar <oboczar@hotmail.com>
2024-10-13 18:23:48 -0700

    Added a Breakable object attachable script

---
+++ Topdown_RPG/Assets/Scripts/BreakableObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+    [SerializeField]
+    private float objectHealth;
+    [SerializeField]
+    private Sprite brokenSprite;
+    private float currentHealth;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
