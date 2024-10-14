247ee666de67f87041e79ab8e1d27cd017bca43c
NomadicCat <55854868+NomadicCat@users.noreply.github.com>
2024-10-13 17:59:11 -0700

    Player Movement and Cinemachine
    Added basic player movement:
        -up down left right
    
    Added Cinemachine to follow player character

---
+++ Topdown_RPG/Assets/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
